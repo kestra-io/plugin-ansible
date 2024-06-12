b8b4217ef24ec0879fc0a638e15e4d76a7dfebb2
brian.mulier <bmmulier@hotmail.fr>
2024-06-12 19:50:54 +0200

    fix(tests): introduce @KestraTest (linked to kestra-io/kestra#3948)

--- src/test/java/io/kestra/plugin/ansible/cli/AnsibleCLITest.java
+++ src/test/java/io/kestra/plugin/ansible/cli/AnsibleCLITest.java
@@ -6,7 +6,7 @@ import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
 import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.kestra.core.junit.annotations.KestraTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +17,7 @@ import java.util.Map;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@MicronautTest
+@KestraTest
 class AnsibleCLITest {
 
     @Inject
