6174c095840707e23162531b5c3a356014b80949
Loïc Mathieu <loikeseke@gmail.com>
2024-06-24 12:30:33 +0200

    feat: use task runner

--- src/main/java/io/kestra/plugin/ansible/cli/AnsibleCLI.java
+++ src/main/java/io/kestra/plugin/ansible/cli/AnsibleCLI.java
@@ -5,12 +5,15 @@ import io.kestra.core.models.annotations.Plugin;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.tasks.*;
 import io.kestra.core.models.tasks.runners.ScriptService;
+import io.kestra.core.models.tasks.runners.TaskRunner;
 import io.kestra.core.runners.RunContext;
 import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
 import io.kestra.plugin.scripts.exec.scripts.models.RunnerType;
 import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
 import io.kestra.plugin.scripts.exec.scripts.runners.CommandsWrapper;
+import io.kestra.plugin.scripts.runner.docker.Docker;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -105,12 +108,25 @@ public class AnsibleCLI extends Task implements RunnableTask<ScriptOutput>, Name
     protected Map<String, String> env;
 
     @Schema(
-        title = "Docker options for the `DOCKER` runner.",
-        defaultValue = "{image=" + DEFAULT_IMAGE + ", pullPolicy=ALWAYS}"
+        title = "Deprecated, use 'taskRunner' instead"
+    )
+    @PluginProperty
+    @Deprecated
+    private DockerOptions docker;
+
+    @Schema(
+        title = "The task runner to use.",
+        description = "Task runners are provided by plugins, each have their own properties."
     )
     @PluginProperty
     @Builder.Default
-    protected DockerOptions docker = DockerOptions.builder().build();
+    @Valid
+    protected TaskRunner taskRunner = Docker.INSTANCE;
+
+    @Schema(title = "The task runner container image, only used if the task runner is container-based.")
+    @PluginProperty(dynamic = true)
+    @Builder.Default
+    protected String containerImage = DEFAULT_IMAGE;
 
     private NamespaceFiles namespaceFiles;
 
@@ -122,8 +138,9 @@ public class AnsibleCLI extends Task implements RunnableTask<ScriptOutput>, Name
     public ScriptOutput run(RunContext runContext) throws Exception {
         CommandsWrapper commandsWrapper = new CommandsWrapper(runContext)
             .withWarningOnStdErr(false)
-            .withRunnerType(RunnerType.DOCKER)
             .withDockerOptions(injectDefaults(docker))
+            .withTaskRunner(this.taskRunner)
+            .withContainerImage(this.containerImage)
             .withCommands(
                 ScriptService.scriptCommands(
                     List.of("/bin/bash", "-c"),
@@ -140,6 +157,10 @@ public class AnsibleCLI extends Task implements RunnableTask<ScriptOutput>, Name
     }
 
     private DockerOptions injectDefaults(DockerOptions original) {
+        if (original == null) {
+            return null;
+        }
+        
         var builder = original.toBuilder();
         if (original.getImage() == null) {
             builder.image(DEFAULT_IMAGE);
