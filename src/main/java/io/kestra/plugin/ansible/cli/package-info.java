3b8404b5ba64d8b4250ef87d638796ee0d71219d
ben8t <pimpaudben@gmail.com>
2025-12-30 15:34:12 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/ansible/cli/package-info.java
+++ src/main/java/io/kestra/plugin/ansible/cli/package-info.java
@@ -1,6 +1,9 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using Ansible CLI.\n",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.ansible;
 
