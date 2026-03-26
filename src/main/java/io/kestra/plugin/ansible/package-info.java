49b066bd2d48744f0e45ae372ee66735b26135d0
Malay Dewangan <malaydewangan310@gmail.com>
2026-03-26 14:18:38 +0530

    fix: add missing package-info

---
+++ src/main/java/io/kestra/plugin/ansible/package-info.java
@@ -0,0 +1,9 @@
+@PluginSubGroup(
+    description = "Tasks that orchestrate Ansible playbooks from Kestra flows.\n",
+    categories = {
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
+)
+package io.kestra.plugin.ansible;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
