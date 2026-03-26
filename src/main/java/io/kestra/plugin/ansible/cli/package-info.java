e621f8dd3c3e79055ccbc9659974424e4131f352
Malay Dewangan <malaydewangan310@gmail.com>
2026-03-26 13:14:24 +0530

    refactor: remove redundant package"

--- src/main/java/io/kestra/plugin/ansible/cli/package-info.java
+++ src/main/java/io/kestra/plugin/ansible/cli/package-info.java
@@ -1,9 +1,9 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for using Ansible CLI.\n",
+    description = "Tasks that run Ansible CLI commands to execute playbooks and capture results.\n",
     categories = {
         PluginSubGroup.PluginCategory.INFRASTRUCTURE
     }
 )
-package io.kestra.plugin.ansible;
+package io.kestra.plugin.ansible.cli;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
